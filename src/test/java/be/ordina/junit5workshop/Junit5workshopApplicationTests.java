973672cb035057cee1cc04039411b74adc8596b7
Tim Schmitte <tim.schmitte@gmail.com>
2018-09-26 14:01:55 +0200

    Removing readme and adding spring context test to allow people checking out the unsolved excercise to at least check if the spring context is ok

---
+++ src/test/java/be/ordina/junit5workshop/Junit5workshopApplicationTests.java
@@ -0,0 +1,17 @@
+package be.ordina.junit5workshop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class Junit5workshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
